94272c7a7bc4b6a954292ae4efc592b87606b0c7
Jedidiah Alfred-Aigbe <jedidiah.alfred-aigbe@stu.ocsb.ca>
2023-02-14 19:35:34 +0000

    added math equations

--- MathOperators.java
+++ MathOperators.java
@@ -12,23 +12,23 @@ class MathOperators {
     * @param args Unused
     */
     public static void main(String[] args) {
-        //Addition
+        // Addition
         System.out.println("0 + 1 =" + (0 + 1));
-        //Subtraction
+        // Subtraction
         System.out.println("6 - 4 =" + (6 - 4));
-        //Multiplication
+        // Multiplication
         System.out.println("3 * 1 =" + (3 * 1));
-        //
+        // Division with a normal output
         System.out.println("8 / 2 =" + (8 / 2));
-        //Division
+        // Division with a decimal behind output
         System.out.println("25 / 5 =" + (25f / 5f));
-        //
+        // Division with a decimal in front output
         System.out.println("4 / 8 =" + (4d / 8d));
-        //
+        // Exponents to the power of a number
         System.out.println("5^2 =" + (Math.pow(5, 2)));
-        //
+        // Exponents to the power of a number
         System.out.println("2^5 =" + (Math.pow(2, 5)));
-        //
+        // Square root
         System.out.println("Square Root of 36 =" + (Math.sqrt(36)));
     }
 }
