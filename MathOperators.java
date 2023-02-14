d84c70077ccc5fd73cc3548846f63b26489cd695
Jedidiah Alfred-Aigbe <jedidiah.alfred-aigbe@stu.ocsb.ca>
2023-02-14 18:58:24 +0000

    added math problems equation

---
+++ MathOperators.java
@@ -0,0 +1,34 @@
+/**
+* This program prints math problems.
+*
+* @author Jedidiah Alfred
+* @version 1.0
+* @since 2023-02-09
+*/
+class MathOperators {
+    /**
+    * This program prints math problems.
+    *
+    * @param args Unused
+    */
+    public static void main(String[] args) {
+        //Addition
+        System.out.println("0 + 1 =" + (0 + 1));
+        //Subtraction
+        System.out.println("6 - 4 =" + (6 - 4));
+        //Multiplication
+        System.out.println("3 * 1 =" + (3 * 1));
+        //
+        System.out.println("8 / 2 =" + (8 / 2));
+        //Division
+        System.out.println("25 / 5 =" + (25f / 5f));
+        //
+        System.out.println("4 / 8 =" + (4d / 8d));
+        //
+        System.out.println("5^2 =" + (Math.pow(5, 2)));
+        //
+        System.out.println("2^5 =" + (Math.pow(2, 5)));
+        //
+        System.out.println("Square Root of 36 =" + (Math.sqrt(36)));
+    }
+}
